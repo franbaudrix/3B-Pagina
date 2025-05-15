6e0e1c2c0ed5176e8cf2e3f1be2f6ed642f7231d
franbaudrix <franco39239@gmail.com>
2025-05-15 19:58:58 -0300

    cambios server

--- server/server.js
+++ server/server.js
@@ -5,12 +5,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
+const path = require('path');
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, '..', 'client')));
 
 // Conexión a MongoDB
 mongoose.connect(process.env.DB_URI)
@@ -35,6 +36,9 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/pedidos', pedidosRoutes);
 app.use('/api/auth', authRoutes);
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'paginaEmpleados', 'index.html'));
+});
 
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
